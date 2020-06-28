0f1c8f008a2603c660ecfb4087b14da3918320fb
ishmamt <55696609+ishmamt@users.noreply.github.com>
2020-06-28 10:55:13 +0600

    Added window to show output

--- classdef.py
+++ classdef.py
@@ -1,5 +1,9 @@
 # Script for all the classes
 
+# imports
+import pygame
+
+
 # constants
 GREEN = (0, 255, 0)
 RED = (255, 0, 0)
@@ -18,8 +22,8 @@ class Node(object):
         self.end = False
         self.neighbours = list()  # list of all the neighbours
         self.gcost = 10000000000  # any high value ie. infinity
-        self.hcost = None  # we don't know yet
-        self.fcost = None
+        self.hcost = None  # heuristic cost
+        self.fcost = None  # addition of gcost & hcost
         self.prev = None  # this represents the parent node that has to be traversed to get to this node
 
     def setStart(self):
@@ -35,15 +39,26 @@ class Node(object):
     def getfcost(self):
         return self.gcost + self.hcost
 
-    def getNeighbours(self):
+    def getNeighbours(self, grid):
         for row in range(-1, 2):
             for col in range(-1, 2):
                 print(row, col)
 
+    def draw(self):
+        pass
 
-# class Grid(object):
-#     # class definition for the grid
-#     def __init__(self, rows, cols):
-#         self.rows = rows
-#         self.cols = cols
-#         # creating the list of nodes in grid
+
+class Grid(object):
+    # class definition for the grid
+    def __init__(self, x, y, num):
+        self.x = x  # x, y is the position of the first node in grid
+        self.y = y
+        self.num = num  # number on nodes in grid. It dictates the width of the nodes
+        self.grid = self.createGrid()
+
+    def createGrid(self):
+        return [[Node() for col in range(self.num)] for row in range(self.num)]  # returns a grid based on the number of nodes
+
+    def makeRandMaze(self):
+        # makes a random maze
+        pass
--- main.py
+++ main.py
@@ -1,9 +1,49 @@
+# this script contains the main loop
+
+
 # imports
 from classdef import Node
+from classdef import Grid
+import pygame
+
 
 # constants
-WIDTH = 100
-HEIGHT = 100
+
+# screen resolution
+WIDTH = 700
+HEIGHT = 700
+
+# colors
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
 
 # creating the grid
-grid = [[Node() for i in range(101)] for j in range(101)]
+mainGrid = Grid(0, 0, 100)
+
+
+# functions
+def redraw():
+    win.fill(BLACK)  # fills the window after each frame
+    pygame.draw.rect(win, WHITE, (0, 0, round(WIDTH * 0.25), HEIGHT))
+    pygame.display.update()
+
+
+# pygaame window
+pygame.init()  # pygame window init
+win = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption('Pathfinder Visualization')
+clk = pygame.time.Clock()
+FPS = 60  # frames per second
+run = True  # for controlling the main loop
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# anyting between the lines are in the main loop
+while run:
+    clk.tick(FPS)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False  # stop the game if user tries to quit
+    redraw()
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+pygame.quit()
